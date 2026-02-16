36d293abe7b9dd3ea4307fa3436fa22af38ee5cd
Clinx01 <bperezlindo68@gmail.com>
2026-02-15 23:01:53 -0300

    5 ejercicios sobre for y listas basicos

---
+++ forANDlistas-ejercicios/4-forANDlist.py
@@ -0,0 +1,5 @@
+list= []
+for i in range(5):
+    dato = int(input("Ingrese un numero: "))
+    list.append(dato)
+print(list)
---
+++ forANDlistas-ejercicios/5-forANDlist.py
@@ -0,0 +1,6 @@
+lista = [1,2,3,4,5]
+for i in range(len(lista)):
+    if lista[i] % 2 == 0:
+        print ("Es par")
+    else:
+        print("No es par")
